bc750cb957d2a249e838a3467f9fb2e43ae826de
ahq <saurav.giri@olivemedia.co>
2018-03-30 14:58:33 +0545

    production url set

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -4,13 +4,13 @@
 	
 	Class BaseRepository{
 		// for production 
-		//protected $base_url = 'https://api.academyhq.com/api/v2'; 
+		protected $base_url = 'https://api.academyhq.com/api/v2'; 
 
 		// for sandbox 
 		//protected $base_url = 'https://api.sandbox.academyhq.olive.media/api/v2'; 
 
 		// for local 
-		protected $base_url = 'http://api.academyhq.localhost/api/v2'; 
+		// protected $base_url = 'http://api.academyhq.localhost/api/v2'; 
 
 		public function __construct()
 		{
