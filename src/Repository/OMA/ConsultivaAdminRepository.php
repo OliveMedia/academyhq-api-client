e7854b8352421718f03f3599927273098b86b0d2
Sudip Koju <sudip.koju@olivemedia.co>
2018-09-19 15:45:39 +0545

    added methods on consultivaadmin repo

--- src/Repository/OMA/ConsultivaAdminRepository.php
+++ src/Repository/OMA/ConsultivaAdminRepository.php
@@ -83,23 +83,24 @@ class ConsultivaAdminRepository extends BaseRepository
         VO\Token $token,
         VO\ApprenticeshipID $apprenticeship_id,
         VO\OrganisationID $organisation_id,
+        VO\MemberID $assessor_id,
         VO\Name $name,
         VO\StringVO $gender,
         VO\StringVO $country_code,
         VO\Integer $mobile_number,
         VO\Email $email,
-        VO\StringVO $image = null,
-        VO\Integer $disability,
-        VO\StringVO $disability_text = null,
-        VO\StringVO $street = null,
+        VO\StringVO $nationality,
+        VO\StringVO $street,
         VO\StringVO $city,
         VO\StringVO $state,
         VO\StringVO $country,
         VO\StringVO $postal_code,
+        VO\Integer $disability,
+        VO\StringVO $image = null,
         VO\StringVO $employment = null,
         VO\StringVO $further_notes = null,
-        VO\StringVO $nationality,
-        VO\StringVO $date_of_birth = null
+        VO\StringVO $date_of_birth = null,
+        VO\StringVO $disability_text = null
     ) {
         $request =new Request(
             new GuzzleClient,
@@ -113,24 +114,25 @@ class ConsultivaAdminRepository extends BaseRepository
         $request_parameters = array(
             'apprenticeship_id' => $apprenticeship_id->__toString(),
             'organisation_id' => $organisation_id->__toString(),
+            'assessor_id' => $assessor_id->__toString(),
             'first_name' => $name->get_first_name()->__toString(),
             'last_name' => $name->get_last_name()->__toString(),
             'gender' => $gender->__toString(),
             'country_code' => $country_code->__toString(),
             'mobile_number' => $mobile_number->__toInteger(),
             'email' => $email->__toString(),
-            'image' => $image ? $image->__toString() : '',
-            'disability' => $disability->__toInteger(),
-            'disability_text' => $disability_text ? $disability_text->__toString() : '',
+            'nationality' => $nationality-> __toString(),
             'street' => $street->__toString(),
             'city' => $city->__toString(),
             'state' => $state->__toString(),
             'country' => $country->__toString(),
             'postal_code' => $postal_code->__toString(),
+            'disability' => $disability->__toInteger(),
+            'image' => $image ? $image->__toString() : '',
             'employment' => $employment ? $employment->__toString() : '',
             'further_notes' => $further_notes ? $further_notes->__toString() : '',
-            'nationality' => $nationality-> __toString(),
             'date_of_birth' => $date_of_birth ? $date_of_birth-> __toString() : '',
+            'disability_text' => $disability_text ? $disability_text->__toString() : '',
         );
 
         $response = $request->send($request_parameters, $header_parameters);
@@ -216,6 +218,86 @@ public function student_program_details(
             
             'occupation_id'=>$occupation_id->__toInteger(),
        );
+        $response = $request->send($request_parameters, $header_parameters);
+
+        $data = $response->get_data();
+
+        return $data;
+    }
+
+    public function create_member(
+        VO\Token $token,
+        VO\Name $name,
+        VO\Email $email,
+        VO\Integer $is_assessor = null,
+        VO\Integer $is_verifier = null,
+        VO\Integer $is_mentor = null
+    ){
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/consultiva/admin/create/member'),
+            new VO\HTTP\Method('POST')
+        );
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+
+        $request_parameters = array(
+            
+            'first_name' => $name->get_first_name()->__toString(),
+            'last_name' => $name->get_last_name()->__toString(),
+            'email' => $email->__toString()
+        );
+
+        if(!is_null($is_assessor)){
+            $request_parameters['is_assessor']=$is_assessor->__toInteger();
+        }
+
+        if(!is_null($is_verifier)){
+            $request_parameters['is_verifier']=$is_verifier->__toInteger();
+        }
+
+        if(!is_null($is_mentor)){
+            $request_parameters['is_mentor']=$is_mentor->__toInteger();
+        }
+
+        $response = $request->send($request_parameters, $header_parameters);
+
+        $data = $response->get_data();
+
+        return $data;
+    }
+
+    public function list_member(
+        VO\Token $token,
+        VO\StringVO $search,
+        VO\Integer $is_assessor = null,
+        VO\Integer $is_verifier = null,
+        VO\Integer $is_mentor = null
+    ){
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/consultiva/admin/list/members'),
+            new VO\HTTP\Method('POST')
+        );
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+
+        $request_parameters = array(
+            'search' => $search->__toString()
+        );
+
+        if(!is_null($is_assessor)){
+            $request_parameters['is_assessor']=$is_assessor->__toInteger();
+        }
+
+        if(!is_null($is_verifier)){
+            $request_parameters['is_verifier']=$is_verifier->__toInteger();
+        }
+
+        if(!is_null($is_mentor)){
+            $request_parameters['is_mentor']=$is_mentor->__toInteger();
+        }
+
         $response = $request->send($request_parameters, $header_parameters);
 
         $data = $response->get_data();
