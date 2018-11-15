8eaedfd0d6476e4a234c78fb70331a4dd0e7ec11
Saurav Giri <saurav.giri@olivemedia.co>
2018-11-15 14:49:10 +0545

    superadmin repo fix;

--- src/Repository/OMA/SuperAdminRepository.php
+++ src/Repository/OMA/SuperAdminRepository.php
@@ -2,45 +2,45 @@
 
 namespace AcademyHQ\API\Repository\OMA;
 
-use AcademyHQ\API\ValueObjects as VO;
-use AcademyHQ\API\HTTP\Request\Request as Request;
-use Guzzle\Http\Client as GuzzleClient;
 use AcademyHQ\API\Common\Credentials;
+use AcademyHQ\API\HTTP\Request\Request as Request;
 use AcademyHQ\API\Repository\BaseRepository;
+use AcademyHQ\API\ValueObjects as VO;
+use Guzzle\Http\Client as GuzzleClient;
 
 class SuperAdminRepository extends BaseRepository
 {
-	public function __construct(Credentials $credentials)
-	{
-		parent::__construct();
-		$this->credentials = $credentials;
-		// $this->base_url .= '/crms';
-	}
+    public function __construct(Credentials $credentials)
+    {
+        parent::__construct();
+        $this->credentials = $credentials;
+        // $this->base_url .= '/crms';
+    }
 
-	public function get_organisations(
-		VO\Integer $current_page,
-		VO\StringVO $search = null
-	){
+    public function get_organisations(
+        VO\Integer $current_page,
+        VO\StringVO $search = null
+    ) {
 
-		$request = new Request(
-			new GuzzleClient,
-			$this->credentials,
-			VO\HTTP\Url::fromNative($this->get_url().'/crms/organisations/get'),
-			new VO\HTTP\Method('POST')
-		);
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url . '/crms/organisations/get'),
+            new VO\HTTP\Method('POST')
+        );
 
-		$request_parameters = array(
-			'current_page' => $current_page->__toInteger()
-		);
+        $request_parameters = array(
+            'current_page' => $current_page->__toInteger(),
+        );
 
-		if($search){
-			$request_parameters['search'] = $search->__toString();
-		}
+        if ($search) {
+            $request_parameters['search'] = $search->__toString();
+        }
 
-		$response = $request->send($request_parameters);
+        $response = $request->send($request_parameters);
 
-		$data = $response->get_data();
+        $data = $response->get_data();
 
-		return $data;
-	}
+        return $data;
+    }
 }
