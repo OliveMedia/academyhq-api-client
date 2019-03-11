9fb4edec71934dfd122fd0c4e738afa009c94555
server-34 <saurav.giri@olivemedia.co>
2019-03-11 19:00:25 +0545

    api updates

---
+++ src/Repository/OMA/MemberRepository.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace AcademyHQ\API\Repository\OMA;
+
+use AcademyHQ\API\ValueObjects as VO;
+use AcademyHQ\API\HTTP\Request\Request as Request;
+use Guzzle\Http\Client as GuzzleClient;
+use AcademyHQ\API\Common\Credentials;
+use AcademyHQ\API\Repository\BaseRepository;
+
+class MemberRepository extends BaseRepository {
+
+	public function __construct(Credentials $credentials)
+	{
+		parent::__construct();
+		$this->credentials = $credentials;
+		$this->base_url .= '/oma';
+	}
+
+
+	public function reset_member_by_email_or_username(
+		VO\Email $email = null,
+		VO\Username $userName = null
+	) {
+		$request = new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/reset/member/by/email/or/username'),
+			new VO\HTTP\Method('POST')
+		);
+
+		$request_parameters = array();
+
+		if(isset($email)) {
+			$request_parameters['email'] = $email->__toEncodedString();
+		}
+
+		if(isset($userName)) {
+			$request_parameters['userName'] = $userName->__toEncodedString();
+		}
+
+		$response = $request->send($request_parameters, null);
+
+		$data = $response->get_data();
+
+		return $data;
+	}
+
+	public function reset_password_by_hash_key_and_password(
+		VO\Token $hash_key = null,
+		VO\Password $password = null,
+		VO\Password $password_confirm = null
+	) {
+		$request = new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/reset/password/by/hash_key/and/password'),
+			new VO\HTTP\Method('POST')
+		);
+
+		$request_parameters = array();
+
+		if(isset($hash_key)) {
+			$request_parameters['hash_key'] = $hash_key->__toEncodedString();
+		}
+
+		if(isset($password)) {
+			$request_parameters['password'] = $password->__toEncodedString();
+		}
+
+		if(isset($password_confirm)) {
+			$request_parameters['password_confirm'] = $password_confirm->__toEncodedString();
+		}
+		$response = $request->send($request_parameters, null);
+
+		$data = $response->get_data();
+
+		return $data;
+	}
+
+}
