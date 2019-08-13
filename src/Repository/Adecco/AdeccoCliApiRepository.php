d17e27a966226df13e2628aab860c76132bce39e
basuregamiolive <basu.regmi@olivemedia.co>
2019-08-13 12:21:09 +0545

    adecco cli task new endpoint integrated

--- src/Repository/Adecco/AdeccoCliApiRepository.php
+++ src/Repository/Adecco/AdeccoCliApiRepository.php
@@ -16,6 +16,49 @@ class AdeccoCliApiRepository extends BaseRepository {
 		$this->credentials = $credentials; 
 	}
 
+	public function fetch_organisation_member_with_inprogress_bundle()
+	{
+		$request = new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/onscensus/organisation/get/members/with/bundle/inprogress'),
+			new VO\HTTP\Method('GET')
+        );
+
+        $header_parameters = array();
+
+        $request_parameters = array();
+
+        $response = $request->send($request_parameters, $header_parameters);
+
+		$data = $response->get_data();
+
+		return $data;
+	}
+
+	public function fetch_organisation_team_member_with_completed_bundle($teamBundleIds)
+	{
+
+		$request = new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/onscensus/organisation/get/members/with/bundle/completed'),
+			new VO\HTTP\Method('GET')
+        );
+
+        $header_parameters = array();
+
+        $requestParameters = array(
+            'team_bundle_ids'    => $teamBundleIds,
+        );
+
+        $response = $request->send($request_parameters, $header_parameters);
+
+		$data = $response->get_data();
+
+		return $data;
+
+	}
 
 	public function fetch_organisation_member_with_no_enrollment() 
 	{
