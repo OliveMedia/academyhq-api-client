c426bef5c0562de1c61aaf0694982ddebf3f4125
basuregamiolive <basu.regmi@olivemedia.co>
2019-09-06 11:26:06 +0545

    adecco organization bundle search api added

--- src/Repository/Adecco/AdeccoAdminApiRepository.php
+++ src/Repository/Adecco/AdeccoAdminApiRepository.php
@@ -96,5 +96,34 @@ class AdeccoAdminApiRepository extends BaseRepository {
 
 	}
 
+	public function searchOrganisationPackage(
+		VO\Token $token,
+        VO\StringVo $search = null,
+        VO\Integer $page = null
+	) {
+
+		$request = new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/onscensus/package/get/search'),
+			new VO\HTTP\Method('GET')
+        );
+
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+		
+		$request_parameters = array();
+
+		$request_parameters['packageName'] = $search->__toString();
+
+		$request_parameters['page'] = $page->__toInteger();
+
+        $response = $request->send($request_parameters, $header_parameters);
+		$data = $response->get_data();
+
+		return $data;
+
+	}
+
+
    
 }
