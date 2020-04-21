03164ae89829134d7d89640e5347f6f112596e0f
Ishwor Prasad Rijal <iprijal@olivemedia.co>
2020-04-21 16:17:48 +0545

    progress calculation api added for oma ...

--- src/Repository/OMA/ConsultivaAdminRepository.php
+++ src/Repository/OMA/ConsultivaAdminRepository.php
@@ -298,6 +298,33 @@ class ConsultivaAdminRepository extends BaseRepository
         return $data;
     }
 
+    /**
+     * @param VO\Token   $token
+     * @param VO\Integer $member_apprenticeship_id
+     *
+     * @return \AcademyHQ\API\HTTP\Response\json
+     * @throws VO\Exception\MethodNotAllowedException
+     * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
+     */
+    public function getProgress(
+        VO\Token $token,
+        VO\Integer $member_apprenticeship_id
+    ) {
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/consultiva/admin/learner/progress'),
+            new VO\HTTP\Method('POST')
+        );
+        $request_parameters = array(
+            'member_apprenticeship_id'  => $member_apprenticeship_id->__toInteger()
+        );
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+        $response = $request->send($request_parameters, $header_parameters);
+        $data = $response->get_data();
+        return $data;
+    }
+
 
 	/**
 	 * @param VO\Token   $token
