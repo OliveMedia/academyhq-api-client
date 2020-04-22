1ad24f755aae6c0df448ff0d3f56be8074cee5a8
Ishwor Prasad Rijal <iprijal@olivemedia.co>
2020-04-22 15:04:45 +0545

    Authentication access changed for method calculating learner progress ...

--- src/Repository/OMA/StudentRepository.php
+++ src/Repository/OMA/StudentRepository.php
@@ -1005,4 +1005,31 @@ class StudentRepository extends BaseRepository
 
 		return $data;
 	}
+
+    /**
+     * @param VO\Token   $token
+     * @param VO\Integer $member_apprenticeship_id
+     *
+     * @return \AcademyHQ\API\HTTP\Response\json
+     * @throws VO\Exception\MethodNotAllowedException
+     * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
+     */
+    public function getProgress(
+        VO\Token $token,
+        VO\Integer $member_apprenticeship_id
+    ) {
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/learner/progress'),
+            new VO\HTTP\Method('POST')
+        );
+        $request_parameters = array(
+            'member_apprenticeship_id'  => $member_apprenticeship_id->__toInteger()
+        );
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+        $response = $request->send($request_parameters, $header_parameters);
+        $data = $response->get_data();
+        return $data;
+    }
 }
