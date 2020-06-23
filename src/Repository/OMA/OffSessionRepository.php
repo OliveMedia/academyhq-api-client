e35784ec629fbba3fb05ff79c5c7a038b49b798d
Prajwal Shrestha <prajwal_stha@yahoo.com>
2020-06-23 17:47:17 +0545

    Add changes for OMA self on-boarding from existing member

--- src/Repository/OMA/OffSessionRepository.php
+++ src/Repository/OMA/OffSessionRepository.php
@@ -31,13 +31,15 @@ class OffSessionRepository extends BaseRepository
 	/**
 	 * Get Program Details
 	 * @param VO\Integer $occupation_id
+	 * @param VO\Integer $employer_id
 	 *
 	 * @return \AcademyHQ\API\HTTP\Response\json
 	 * @throws VO\Exception\MethodNotAllowedException
 	 * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
 	 */
 	public function get_occupation_details(
-		VO\Integer $occupation_id
+		VO\Integer $occupation_id,
+		VO\Integer $employer_id
 	) {
 		$request = new Request(
 			new GuzzleClient,
@@ -46,7 +48,8 @@ class OffSessionRepository extends BaseRepository
 			new VO\HTTP\Method('POST')
 		);
 		$request_parameters = array(
-			'occupation_id' => $occupation_id->__toInteger(),
+			'occupation_id'     => $occupation_id->__toInteger(),
+			'organisation_id'   => $employer_id->__toInteger(),
 		);
 		$response = $request->send($request_parameters, null);
 		$data = $response->get_data();
@@ -231,5 +234,138 @@ class OffSessionRepository extends BaseRepository
 		return $data;
 
 	}
+
+	/**
+	 * Create Member Apprenticeships
+	 * @param VO\ApprenticeshipID $apprenticeship_id
+	 * @param VO\OrganisationID   $organisation_id
+	 * @param VO\MemberID         $member_id
+	 * @param VO\MemberID|null    $assessor_id
+	 * @param VO\StringVO         $gender
+	 * @param VO\StringVO         $country_code
+	 * @param VO\Integer          $mobile_number
+	 * @param VO\StringVO         $nationality
+	 * @param VO\StringVO|null    $street
+	 * @param VO\StringVO|null    $city
+	 * @param VO\StringVO|null    $state
+	 * @param VO\StringVO|null    $country
+	 * @param VO\StringVO|null    $postal_code
+	 * @param VO\Integer          $disability
+	 * @param VO\StringVO|null    $image
+	 * @param VO\MemberID|null    $verifier_id
+	 * @param VO\StringVO|null    $employment
+	 * @param VO\StringVO|null    $further_notes
+	 * @param VO\StringVO|null    $disability_text
+	 * @param VO\Integer|null     $weekly_learning_hours
+	 *
+	 * @return \AcademyHQ\API\HTTP\Response\json
+	 * @throws VO\Exception\MethodNotAllowedException
+	 * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
+	 */
+	public function create_student_from_member(
+		VO\ApprenticeshipID $apprenticeship_id,
+		VO\OrganisationID $organisation_id,
+		VO\MemberID $member_id,
+		VO\StringVO $gender,
+		VO\StringVO $country_code,
+		VO\Integer $mobile_number,
+		VO\StringVO $nationality,
+		VO\Integer $disability,
+		VO\MemberID $assessor_id = null,
+		VO\MemberID $verifier_id = null,
+		VO\StringVO $street = null,
+		VO\StringVO $city = null,
+		VO\StringVO $state = null,
+		VO\StringVO $country = null,
+		VO\StringVO $postal_code = null,
+		VO\StringVO $image = null,
+		VO\StringVO $employment = null,
+		VO\StringVO $further_notes = null,
+		VO\StringVO $disability_text = null,
+		VO\Integer $weekly_learning_hours=null
+	) {
+		$request =new Request(
+			new GuzzleClient,
+			$this->credentials,
+			VO\HTTP\Url::fromNative($this->base_url.'/out_session/create/member/apprenticeship'),
+			new VO\HTTP\Method('POST')
+		);
+
+		$request_parameters = array(
+			'apprenticeship_id' => $apprenticeship_id->__toString(),
+			'organisation_id'   => $organisation_id->__toString(),
+			'member_id'         => $member_id->__toString(),
+			'nationality'       => $nationality-> __toString(),
+			'disability'        => $disability->__toInteger()
+		);
+
+		if(!is_null($street)){
+			$request_parameters['street'] = $street->__toString();
+		}
+
+		if(!is_null($city)){
+			$request_parameters['city'] = $city->__toString();
+		}
+
+		if(!is_null($state)){
+			$request_parameters['state'] = $state->__toString();
+		}
+
+		if(!is_null($country)){
+			$request_parameters['country'] = $country->__toString();
+		}
+
+		if(!is_null($postal_code)){
+			$request_parameters['postal_code'] = $postal_code->__toString();
+		}
+
+		if(!is_null($image)){
+			$request_parameters['image'] = $image->__toString();
+		}
+
+		if(!is_null($assessor_id)){
+			$request_parameters['assessor_id'] = $assessor_id->__toString();
+		}
+
+		if(!is_null($verifier_id)){
+			$request_parameters['verifier_id'] = $verifier_id->__toString();
+		}
+
+		if(!is_null($gender)){
+			$request_parameters['gender'] = $gender->__toString();
+		}
+
+		if(!is_null($country_code)){
+			$request_parameters['country_code'] = $country_code->__toString();
+		}
+
+
+		if(!is_null($mobile_number)){
+			$request_parameters['mobile_number'] = $mobile_number->__toInteger();
+		}
+
+		if(!is_null($employment)){
+			$request_parameters['employment'] = $employment->__toString();
+		}
+
+		if(!is_null($further_notes)){
+			$request_parameters['further_notes'] = $further_notes->__toString();
+		}
+
+		if(!is_null($disability_text)){
+			$request_parameters['disability_text'] = $disability_text->__toString();
+		}
+
+		if(!is_null($weekly_learning_hours)){
+			$request_parameters['weekly_learning_hours'] = $weekly_learning_hours->__toInteger();
+		}
+
+		$response = $request->send($request_parameters, null);
+
+		$data = $response->get_data();
+
+		return $data;
+	}
+
 
 }
