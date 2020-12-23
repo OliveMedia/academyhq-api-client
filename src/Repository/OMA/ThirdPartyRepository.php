dcb6770492e842c42b0aa0e3c01f93b26cfdc037
Ishwor <ishwor.rijal@olivegroup.io>
2020-12-23 21:24:09 +0545

    Method for listing programs for third party api in oma

--- src/Repository/OMA/ThirdPartyRepository.php
+++ src/Repository/OMA/ThirdPartyRepository.php
@@ -69,4 +69,63 @@ class ThirdPartyRepository extends BaseRepository
 
         return $data;
     }
+
+    /*
+    Parameters :
+        @current_page : num | null
+        @per_page : num | null
+        @direction : asc | desc | null
+        @sort_by : name | created_at | null
+        @search : employer's name | null
+        @status : active | inactive | null
+        @is_published : 1 | 0 | null
+    */
+    public function listPrograms(
+        VO\Integer $current_page,
+        VO\Integer $per_page,
+        VO\StringVO $direction = null,
+        VO\StringVO $sort_by = null,
+        VO\StringVO $search = null,
+        VO\StringVO $status = null,
+        VO\StringVO $is_published = null
+    ) {
+
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url . '/list/programs'),
+            new VO\HTTP\Method('POST')
+        );
+
+        $request_parameters = array(
+            'current_page' => $current_page->__toInteger(),
+            'per_page' => $per_page->__toInteger(),
+        );
+
+        if ($direction) {
+            $request_parameters['direction'] = $direction->__toString();
+        }
+
+        if ($sort_by) {
+            $request_parameters['sort_by'] = $sort_by->__toString();
+        }
+
+        if ($search) {
+            $request_parameters['search'] = $search->__toString();
+        }
+
+        if ($status) {
+            $request_parameters['status'] = $status->__toString();
+        }
+
+        if (!is_null($is_published)) {
+            $request_parameters['is_published'] = $is_published->__toString();
+        }
+
+        $response = $request->send($request_parameters);
+
+        $data = $response->get_data();
+
+        return $data;
+    }
 }
