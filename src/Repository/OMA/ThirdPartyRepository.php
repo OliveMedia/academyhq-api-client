5cc46dcb51ada601c5337ebd4fbf8322e4fa713e
Ishwor <ishwor.rijal@olivegroup.io>
2021-01-08 11:20:15 +0545

    list apprentice programs for third api

--- src/Repository/OMA/ThirdPartyRepository.php
+++ src/Repository/OMA/ThirdPartyRepository.php
@@ -241,4 +241,57 @@ class ThirdPartyRepository extends BaseRepository
 
         return $data;
     }
+
+    /*
+    Parameters :
+        @current_page : num | null
+        @per_page : num | null
+        @direction : asc | desc | null
+        @sort_by : name | created_at | null
+        @search : employer's name | null
+        @member : member_id
+    */
+    public function listApprenticePrograms(
+        VO\Integer $current_page,
+        VO\Integer $per_page,
+        VO\StringVO $direction = null,
+        VO\StringVO $sort_by = null,
+        VO\StringVO $search = null,
+        VO\StringVO $member = null
+    ) {
+
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url . '/list/apprentice/programs'),
+            new VO\HTTP\Method('POST')
+        );
+
+        $request_parameters = array(
+            'current_page' => $current_page->__toInteger(),
+            'per_page' => $per_page->__toInteger(),
+        );
+
+        if ($direction) {
+            $request_parameters['direction'] = $direction->__toString();
+        }
+
+        if ($sort_by) {
+            $request_parameters['sort_by'] = $sort_by->__toString();
+        }
+
+        if ($search) {
+            $request_parameters['search'] = $search->__toString();
+        }
+
+        if ($member) {
+            $request_parameters['member'] = $member->__toString();
+        }
+
+        $response = $request->send($request_parameters);
+
+        $data = $response->get_data();
+
+        return $data;
+    }
 }
