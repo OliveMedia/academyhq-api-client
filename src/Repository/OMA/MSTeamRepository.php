14350b7389177af509cfd6b4d9a6b34443a9caf1
lekshmi-kumar <76201648+lekshmi-kumar@users.noreply.github.com>
2021-03-10 12:28:10 +0530

    list_all_employers

--- src/Repository/OMA/MSTeamRepository.php
+++ src/Repository/OMA/MSTeamRepository.php
@@ -174,5 +174,38 @@ class MSTeamRepository extends BaseRepository
 
         return $data;
     }
+    /**
+     * List Employer
+     * @param VO\Token         $token
+     * @param VO\StringVO|null $search
+     * @param VO\Integer       $current_page
+     *
+     * @return \AcademyHQ\API\HTTP\Response\json
+     * @throws VO\Exception\MethodNotAllowedException
+     * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
+     */
+    public function ListConsultiva(
+        VO\Token $token,
+        VO\StringVO $search = null,
+        VO\Integer $current_page
+    ) {
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/list/employees'),
+            new VO\HTTP\Method('POST')
+        );
 
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+
+        $request_parameters = array(
+            'search'        => $search ? $search->__toString() : '',
+            'current_page'  => $current_page->__toInteger()
+        );
+        $response = $request->send($request_parameters, $header_parameters);
+
+        $data = $response->get_data();
+
+        return $data;
+    }
 }
