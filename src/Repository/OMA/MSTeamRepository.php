7685150618bd0fe80a6d46a7ded1d52fbfefb310
lekshmi-kumar <76201648+lekshmi-kumar@users.noreply.github.com>
2021-04-15 13:12:39 +0530

    Update MSTeamRepository.php

--- src/Repository/OMA/MSTeamRepository.php
+++ src/Repository/OMA/MSTeamRepository.php
@@ -390,5 +390,31 @@ class MSTeamRepository extends BaseRepository
         $data = $response->get_data();
         return $data;
     }
+/**
+     * @param VO\Token   $token
+     * @param VO\Integer $member_apprenticeship_id
+     *
+     * @return \AcademyHQ\API\HTTP\Response\json
+     * @throws VO\Exception\MethodNotAllowedException
+     * @throws \AcademyHQ\API\HTTP\Response\Exception\ResponseException
+     */
+    public function getlastAssessedAtWithMemberID(
+        VO\Token $token,
+        VO\Integer $member_id
+    ) {
+        $request = new Request(
+            new GuzzleClient,
+            $this->credentials,
+            VO\HTTP\Url::fromNative($this->base_url.'/getlastassessedatwithmemberID'),
+            new VO\HTTP\Method('POST')
+        );
+        $request_parameters = array(
+            'member_id'  => $member_id->__toInteger()
+        );
+        $header_parameters = array('Authorization' => $token->__toEncodedString());
+        $response = $request->send($request_parameters, $header_parameters);
+        $data = $response->get_data();
+        return $data;
+    }
 
 }
