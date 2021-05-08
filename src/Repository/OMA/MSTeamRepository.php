7a919ad6eccc29b2a27ed023139bd28888771232
lekshmi.kumar <lekshmi.kumar@quinoid.in>
2021-05-08 18:43:58 +0530

    Update MSTeamRepository.php

--- src/Repository/OMA/MSTeamRepository.php
+++ src/Repository/OMA/MSTeamRepository.php
@@ -631,5 +631,67 @@ class MSTeamRepository extends BaseRepository
         $response = $request->send($request_parameters, $header_parameters);
         return $response->get_data();
     }
+    /**
+    * Create New Client with completely registered admin
+    **/
+
+    public function createThirdPartyClient(
+        VO\StringVO $name,
+        VO\StringVO $email,
+        VO\StringVO $password,
+        VO\StringVO $mobile_number,
+        VO\StringVO $profile_picture,
+        VO\StringVO $company_name,
+        VO\StringVO $branding_logo_url,
+        VO\StringVO $background_url,
+        VO\StringVO $branding_hex,
+        VO\StringVO $domain,
+        VO\StringVO $plan = null
+        ){
+            $request = new Request(
+                new GuzzleClient,
+                $this->credentials,
+                VO\HTTP\Url::fromNative($this->base_url . '/create-third-party-client'),
+                new VO\HTTP\Method('POST')
+            );
+
+            if(!is_null($name)){
+                $request_parameters['name'] = $name->__toString();
+            }
+            if(!is_null($email)){
+                $request_parameters['email'] = $email->__toString();
+            }
+            if(!is_null($password)){
+                $request_parameters['password'] = $password->__toString();
+            }
+            if(!is_null($mobile_number)){
+                $request_parameters['mobile_number'] = $mobile_number->__toString();
+            }
+            if(!is_null($profile_picture)){
+                $request_parameters['profile_picture'] = $profile_picture->__toString();
+            }
+            if(!is_null($company_name)){
+                $request_parameters['company_name'] = $company_name->__toString();
+            }
+            if(!is_null($branding_logo_url)){
+                $request_parameters['branding_logo_url'] = $branding_logo_url->__toString();
+            }
+            if(!is_null($background_url)){
+                $request_parameters['background_url'] = $background_url->__toString();
+            }
+            if(!is_null($branding_hex)){
+                $request_parameters['branding_hex'] = $branding_hex->__toString();
+            }
+            if(!is_null($domain)){
+                $request_parameters['domain'] = $domain->__toString();
+            }
+            if(!is_null($plan)){
+                $request_parameters['plan'] = $plan->__toString();
+            }
+
+            $response = $request->send($request_parameters);
+            $data = $response->get_data();
+            return $data;
+    }
 
 }
