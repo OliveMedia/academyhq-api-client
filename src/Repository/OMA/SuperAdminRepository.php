3497d6088c3c7c901d2ee56c6994647297d5af24
lekshmi.kumar <lekshmi.kumar@quinoid.in>
2021-06-07 11:31:26 +0530

    Update SuperAdminRepository.php

--- src/Repository/OMA/SuperAdminRepository.php
+++ src/Repository/OMA/SuperAdminRepository.php
@@ -283,7 +283,7 @@ class SuperAdminRepository extends BaseRepository
 /*
     * Get list of all Apprenticeship Client across which the Learner is registered in
     */
-    public function list_main_organisation(
+    public function list_organisation_based_on_email(
         VO\Integer $current_page,
         VO\StringVO $search = null,
         VO\StringVO $email,
